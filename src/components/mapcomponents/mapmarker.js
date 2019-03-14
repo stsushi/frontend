c326f469cb271aac57928e0d0063f3c9e72b8248
Andy Lo <andylo@ocf.berkeley.edu>
2019-03-13 18:18:24 -0700

    added format for heur

--- src/components/mapcomponents/mapmarker.js
+++ src/components/mapcomponents/mapmarker.js
@@ -12,6 +12,56 @@ class MapMarker extends Component {
     componentDidMount() {
         this.getExtract(this.props.title);
     }
+    icontype(data, title) {
+        if (Mbattle(data, title)==true){    
+            const icon = L.icon({
+                iconRetinaUrl: require('leaflet/dist/images/battle-marker-icon-2x.png'),
+                iconUrl: require('leaflet/dist/images/battle-marker-icon.png'),
+                shadowUrl: require('leaflet/dist/images/battle-marker-shadow.png')
+            });
+            return(L.icon)
+        }
+        if (Mevent(data, title)==true){    
+            const icon = L.icon({
+                iconRetinaUrl: require('leaflet/dist/images/event-marker-icon-2x.png'),
+                iconUrl: require('leaflet/dist/images/event-marker-icon.png'),
+                shadowUrl: require('leaflet/dist/images/event-marker-shadow.png')
+            });
+            return(L.icon)
+        }
+        if (Mlocation(data, title)==true){    
+            const icon = L.icon({
+                iconRetinaUrl: require('leaflet/dist/images/local-marker-icon-2x.png'),
+                iconUrl: require('leaflet/dist/images/local-marker-icon.png'),
+                shadowUrl: require('leaflet/dist/images/loca-marker-shadow.png')
+            });
+            return(L.icon)
+        }
+        if (Mperson(data, title)==true){    
+            const icon = L.icon({
+                iconRetinaUrl: require('leaflet/dist/images/person-marker-icon-2x.png'),
+                iconUrl: require('leaflet/dist/images/person-marker-icon.png'),
+                shadowUrl: require('leaflet/dist/images/person-marker-shadow.png')
+            });
+            return(L.icon)
+        }
+    
+    }
+    Mbattle(data, title) {
+        string = title.concat(" ")
+        string = string.concat(data)
+        words = string.split(" ")
+        const battlewords = ["skirmish","battle","fight","army","Skirmish","Battle","Fight","Army"]
+        for (i in string) {
+            for(j in battlewords) {
+                if (string[i].localeCompare(battlewords[j])==0) {
+                    return(true)
+                }
+            }
+        }
+        
+    }
+    // add more of the same
 
     getExtract = title => {
         title = title.replace(/ /g, "_");
@@ -19,17 +69,13 @@ class MapMarker extends Component {
             .then(response => response.json())
             .then(data => {
                 this.setState({extract: data.extract});
+                this.setState({icon: icontype(data.extract,data.title)});
             });
     }
 
     render () {
-        const icon = L.icon({
-            iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-            iconUrl: require('leaflet/dist/images/marker-icon.png'),
-            shadowUrl: require('leaflet/dist/images/marker-shadow.png')
-        });
         return(
-            <Marker position={this.props.position} icon={icon}
+            <Marker position={this.props.position} icon={this.state.icon}
                     onMouseOver={e => {
                         e.target.openPopup();
                     }}
